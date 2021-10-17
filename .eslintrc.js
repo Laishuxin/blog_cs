bfe8dd9983153758fa543cd700751ac8cb4175fa
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-10-17 19:47:00 +0800

    ci: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  plugins: ['@babel/eslint-parser'],
+}
--- docs/.vuepress/config/sidebar_config.js
+++ docs/.vuepress/config/sidebar_config.js
@@ -2,21 +2,31 @@ const { getFileNames, getFileNamesByGroups } = require('./utils')
 const { resolve } = require('path')
 const basePath = resolve(__dirname, '../../')
 
-const cate = ['csapp', 'design-patterns', 'computer-network']
+const cate = [
+  'csapp',
+  'data-structure-and-algorithm',
+  'design-patterns',
+  'computer-network',
+]
 const getSidebarName = name => `/cate/${name}/`
 
 const sidebar = {
-  [getSidebarName('csapp')]: getCsapp(
+  [getSidebarName('csapp')]: getGroups(
+    './cate/csapp/',
     'Part I: Program Structure and Execution',
+  ),
+  [getSidebarName('data-structure-and-algorithm')]: getGroups(
+    './cate/data-structure-and-algorithm',
+    'algorithms',
   ),
   [getSidebarName('design-patterns')]: getDesignPatterns(),
   [getSidebarName('computer-network')]: getFilesByName('computer-network'),
   '/cate/': cate,
 }
 
-function getCsapp(...groups) {
-  const path = resolve(basePath, './cate/csapp/')
-  const group = getFileNamesByGroups(path, groups)
+function getGroups(path, ...groups) {
+  const _path = resolve(basePath, path)
+  const group = getFileNamesByGroups(_path, groups)
   return groups.map(item => {
     return {
       title: item,
@@ -25,6 +35,17 @@ function getCsapp(...groups) {
     }
   })
 }
+// function getCsapp(...groups) {
+//   const path = resolve(basePath, './cate/csapp/')
+//   const group = getFileNamesByGroups(path, groups)
+//   return groups.map(item => {
+//     return {
+//       title: item,
+//       collapsable: false,
+//       children: group[item],
+//     }
+//   })
+// }
 
 function getDesignPatterns() {
   const path = resolve(basePath, './cate/design-patterns')
